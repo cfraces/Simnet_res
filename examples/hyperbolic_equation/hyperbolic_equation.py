afdceb9bba8f801f93108ad48d6c8d3c0ed77fdd
cfraces <fracescedric@gmail.com>
2021-04-20 20:50:27 -0700

    Added buckley_1d_parameterized.py for parametrization of buckley on Darcy velocity (ud)

--- examples/hyperbolic_equation/hyperbolic_equation.py
+++ examples/hyperbolic_equation/hyperbolic_equation.py
@@ -88,6 +88,86 @@ class BuckleyEquation(PDES):
                                           + (c * u).diff(z))
 
 
+class BuckleyEquationParam(PDES):
+  """
+  Buckley Leverett equation
+
+  Parameters
+  ==========
+  u : str
+      The dependent variable.
+  c : float, Sympy Symbol/Expr, str
+      Wave speed coefficient. If `c` is a str then it is
+      converted to Sympy Function of form 'c(x,y,z,t)'.
+      If 'c' is a Sympy Symbol or Expression then this
+      is substituted into the equation.
+  dim : int
+      Dimension of the wave equation (1, 2, or 3). Default is 2.
+  time : bool
+      If time-dependent equations or not. Default is True.
+
+  Examples
+  ========
+  >>> we = BuckleyEquation(c=0.8, dim=3)
+  >>> we.pprint(preview=False)
+    wave_equation: u__t__t - 0.64*u__x__x - 0.64*u__y__y - 0.64*u__z__z
+    buckley_equation: u__t - 0.8*u__x - 0.8*u__y - 0.8*u__z
+  >>> we = BuckleyEquation(c='c', dim=2, time=False)
+  >>> we.pprint(preview=False)
+    buckley_equation: -c*u__x__x - c*u__y - c*c__x*u__x - c*c__y*u__y
+  """
+
+  name = 'BuckleyLeverettEquationParam'
+
+  def __init__(self, u='u', c='c', dim=3, time=True, eps=1e-2):
+    # set params
+    self.u = u
+    self.dim = dim
+    self.time = time
+
+    # coordinates
+    x, y, z = Symbol('x'), Symbol('y'), Symbol('z')
+
+    # time
+    t = Symbol('t')
+
+    # make input variables
+    input_variables = {'x': x, 'y': y, 'z': z, 't': t}
+    if self.dim == 1:
+      input_variables.pop('y')
+      input_variables.pop('z')
+    elif self.dim == 2:
+      input_variables.pop('z')
+    if not self.time:
+      input_variables.pop('t')
+
+    # Scalar function
+    assert type(u) == str, "u needs to be string"
+    u = Function(u)(*input_variables)
+
+    # wave speed coefficient
+    if type(c) is str:
+      c = Function(c)(*input_variables)
+    elif type(c) in [float, int]:
+      c = Number(c)
+
+    # set equations
+    self.equations = {}
+
+    # Piecewise f
+    # f = Max(-(1.7075*u - 0.3415)*(Heaviside(u - 0.6597) - 1) + 2*(u - 0.2)**2*Heaviside(u - 0.6597)/(2*(u - 0.2)**2 + (u - 1)**2), 0)
+    f = Max(-(1.366025403514163 * u) * (Heaviside(u - 0.577357735773577) - 1)
+            + 2 * (u ** 2) * Heaviside(u - 0.577357735773577) / (2 * (u) ** 2 + (u - 1) ** 2), 0)
+
+    # True f
+    # f = (u - c) * (u - c) / ((u - c) ** 2 + (1 - u) * (1 - u) / 2)
+
+    self.equations['buckley_equation_param'] = (u.diff(t)
+                                                + c * f.diff(x).replace(DiracDelta, lambda x: 0)
+                                                + (c * u).diff(y)
+                                                + (c * u).diff(z))
+
+
 class BuckleyEquationWeighted(PDES):
   name = 'WeightedBuckleyLeverettEquation'
 
@@ -395,8 +475,8 @@ class GravitySegregationWeighted(PDES):
     rhoo = 40  # Oil
     rhow = 62.238  # Water
     # Viscosities
-    muo = 1#2e-4  # lb/ft-s
-    muw = 5#6e-6
+    muo = 1  # 2e-4  # lb/ft-s
+    muw = 5  # 6e-6
     conv = 9.1688e-8  # md to ft2
     fw = lambda S: krw(S) * kro(S) / (kro(S) + krw(S) * muo / muw)
     f = fw(sw)
